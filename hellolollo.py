452956396e964e96eefcf86e2726baa47dfd8734
Ikmrv333 <ivkostomarov@icloud.com>
2026-09-15 13:03:38 +0300

    Add files via upload

---
+++ hellolollo.py
@@ -0,0 +1,26 @@
+'''
+import math
+def PowerA3(A):
+    B = math.pow(A, 2)
+    C = math.pow(A, 3)
+    D = math.pow(A, 4)
+    return B, C, D
+
+print(PowerA3(2))'''
+
+'''
+def Mean(X, Y):
+    AMean = (X + Y) / 2
+    GMean = (X * Y) ** 0.5
+    return AMean, GMean
+
+print(Mean(4, 8))'''
+
+'''
+def TrianglePS(a):
+    P = (3 * a)
+    S = (a ** 2 * (3/4) ** 0.5)
+    return P, S
+
+print(TrianglePS(4))'''
+
